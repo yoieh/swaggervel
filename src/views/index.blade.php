5bdb87da25286fb0734a09aa90fed525c8d16b07
Florian Greinus <florian.greinus@gmail.com>
2017-01-28 01:04:19 +0100

    css and js must be included through relative path (idea/code by @mkgareja)

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,47 +1,46 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-
 header('Access-Control-Allow-Methods: GET, POST');
-
 header("Access-Control-Allow-Headers: X-Requested-With");
 ?>
-        <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="IE=edge">
     <title>Swagger UI</title>
-    <link rel="icon" type="image/png" href="/vendor/swaggervel/images/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="/vendor/swaggervel/images/favicon-16x16.png" sizes="16x16" />
-    <link href='/vendor/swaggervel/css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='/vendor/swaggervel/css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='/vendor/swaggervel/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='/vendor/swaggervel/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
-    <link href='/vendor/swaggervel/css/print.css' media='print' rel='stylesheet' type='text/css'/>
+    <link rel="icon" type="image/png" href="{{ url('/vendor/swaggervel/images/favicon-32x32.png') }}" sizes="32x32"/>
+    <link rel="icon" type="image/png" href="{{ url('/vendor/swaggervel/images/favicon-16x16.png') }}" sizes="16x16"/>
+    <link href='{{ url('/vendor/swaggervel/css/typography.css') }}' media='screen' rel='stylesheet' type='text/css'/>
+    <link href='{{ url('/vendor/swaggervel/css/reset.css') }}' media='screen' rel='stylesheet' type='text/css'/>
+    <link href='{{ url('/vendor/swaggervel/css/screen.css') }}' media='screen' rel='stylesheet' type='text/css'/>
+    <link href='{{ url('/vendor/swaggervel/css/reset.css') }}' media='print' rel='stylesheet' type='text/css'/>
+    <link href='{{ url('/vendor/swaggervel/css/print.css') }}' media='print' rel='stylesheet' type='text/css'/>
     <style type="text/css">
         #oauth2-settings {
             margin-top: 10px;
         }
+
         #oauth2-settings .note {
             font-size: smaller;
             color: #919191;
         }
     </style>
 
-    <script src='/vendor/swaggervel/lib/object-assign-pollyfill.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/jquery.slideto.min.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/jquery.wiggle.min.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/handlebars-4.0.5.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/lodash.min.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/backbone-min.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/swagger-ui.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/highlight.9.1.0.pack.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/highlight.9.1.0.pack_extended.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/jsoneditor.min.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/marked.js' type='text/javascript'></script>
-    <script src='/vendor/swaggervel/lib/swagger-oauth.js' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/object-assign-pollyfill.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/jquery-1.8.0.min.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/jquery.slideto.min.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/jquery.wiggle.min.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/jquery.ba-bbq.min.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/handlebars-4.0.5.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/lodash.min.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/backbone-min.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/swagger-ui.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/highlight.9.1.0.pack.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/highlight.9.1.0.pack_extended.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/jsoneditor.min.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/marked.js') }}' type='text/javascript'></script>
+    <script src='{{ url('/vendor/swaggervel/lib/swagger-oauth.js') }}' type='text/javascript'></script>
 
     <!-- Some basic translations -->
     <!-- <script src='lang/translator.js' type='text/javascript'></script> -->
@@ -83,15 +82,15 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
                     if (typeof initOAuth == "function") {
                         initOAuth({
-                            clientId: "{!! $clientId !!}"||"my-client-id",
-                            clientSecret: "{!! $clientSecret !!}"||"_",
-                            realm: "{!! $realm !!}"||"_",
-                            appName: "{!! $appName !!}"||"_",
+                            clientId: "{!! $clientId !!}" || "my-client-id",
+                            clientSecret: "{!! $clientSecret !!}" || "_",
+                            realm: "{!! $realm !!}" || "_",
+                            appName: "{!! $appName !!}" || "_",
                             scopeSeparator: ","
                         });
 
                         window.oAuthRedirectUrl = "{{ url('vendor/swaggervel/o2c.html') }}";
-                        $('#clientId').html("{!! $clientId !!}"||"my-client-id");
+                        $('#clientId').html("{!! $clientId !!}" || "my-client-id");
                         $('#redirectUrl').html(window.oAuthRedirectUrl);
                     }
 
@@ -127,20 +126,18 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
             $('#input_apiKey').change(addApiKeyAuthorization);
 
-            $('#init-oauth').click(function(){
+            $('#init-oauth').click(function () {
                 if (typeof initOAuth == "function") {
                     initOAuth({
-                        clientId: $('#input_clientId').val()||"my-client-id",
-                        clientSecret: $('#input_clientSecret').val()||"_",
-                        realm: $('#input_realm').val()||"_",
-                        appName: $('#input_appName').val()||"_",
+                        clientId: $('#input_clientId').val() || "my-client-id",
+                        clientSecret: $('#input_clientSecret').val() || "_",
+                        realm: $('#input_realm').val() || "_",
+                        appName: $('#input_appName').val() || "_",
                         scopeSeparator: ","
                     });
                 }
             });
-
             window.swaggerUi.load();
-
         });
     </script>
 </head>
@@ -148,9 +145,12 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 <body class="swagger-section">
 <div id='header'>
     <div class="swagger-ui-wrap">
-        <a id="logo" href="http://swagger.io"><img class="logo__img" alt="swagger" height="30" width="30" src="/vendor/swaggervel/images/logo_small.png" /><span class="logo__title">swagger</span></a>
+        <a id="logo" href="http://swagger.io"><img class="logo__img" alt="swagger" height="30" width="30"
+                                                   src="{{ url('/vendor/swaggervel/images/logo_small.png') }}"/><span
+                    class="logo__title">swagger</span></a>
         <form id='api_selector'>
-            <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
+            <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl"
+                                      type="text"/></div>
             <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
             <div class='input'><a id="explore" class="header__btn" href="#" data-sw-translate>Explore</a></div>
         </form>
@@ -160,12 +160,14 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 <div id="oauth2-settings" class="swagger-ui-wrap">
     <h2>OAuth2 settings</h2>
     <input placeholder="Client Id" id="input_clientId" name="clientId" type="text" value="{!! $clientId !!}"/>
-    <input placeholder="Client Secret" id="input_clientSecret" name="clientSecret" type="text" value="{!! $clientSecret !!}"/>
+    <input placeholder="Client Secret" id="input_clientSecret" name="clientSecret" type="text"
+           value="{!! $clientSecret !!}"/>
     <input placeholder="Realm" id="input_realm" name="realm" type="text" value="{!! $realm !!}"/>
     <input placeholder="App Name" id="input_appName" name="appName" type="text" value="{!! $appName !!}"/>
     <button id="init-oauth" href="#" data-sw-translate value="">Init OAuth</button>
     <br/>
-    <span class="note">Configure OAuth2 Client with id="<span id="clientId"></span>" and redirect url="<span id="redirectUrl"></span>"</span>
+    <span class="note">Configure OAuth2 Client with id="<span id="clientId"></span>" and redirect url="<span
+                id="redirectUrl"></span>"</span>
 </div>
 
 <div id="message-bar" class="swagger-ui-wrap" data-sw-translate>&nbsp;</div>
